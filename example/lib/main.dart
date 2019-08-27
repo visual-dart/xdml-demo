cb6fec51f3852799c84a4abdd7dc2358a3971c73
Big Mogician <sunaoran@youzan.com>
2019-08-27 14:28:10 +0800

    fix: add project demo

---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:xdml/metadata.dart';
+import 'package:demo/demo.dart';
+import 'package:flutter/material.dart';
+import 'main.binding.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+@Binding('main.xaml')
+class MyAppState extends State<MyApp> {
+  String platformVersion = 'Unknown';
+  final String titleText = 'Hello World';
+  int _inner(int v) {
+    return 555555 + v;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    try {
+      platformVersion = await Demo.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+    setState(() {
+      platformVersion = platformVersion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var a = {"a": 23, "b": "2342"};
+    print(_inner(a['a']));
+    var list = [1, 2, 3, 4, 5, 6, 7, 8];
+    return bindXDML(this, context, a, list);
+  }
+}
